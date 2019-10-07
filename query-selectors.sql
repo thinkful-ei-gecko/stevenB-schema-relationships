91693bd037327031a1c5573545f28130eb633566
Steven Bull <stevenwbulljr@gmail.com>
2019-10-07 13:23:26 -0500

    finish second query

--- query-selectors.sql
+++ query-selectors.sql
@@ -12,6 +12,21 @@ WHERE
   d.dept_name = 'Sales';
 
 /* Second Query */
+SELECT
+  e.emp_name,
+  p.project_name
+FROM
+  employee e
+  JOIN
+  employee_project ep
+  ON 
+    ep.emp_id = e.id
+  JOIN 
+  project p
+  ON 
+    ep.project_id = p.id
+WHERE 
+  p.project_name = 'Plan christmas party';
 
 /* Third Query */
 
