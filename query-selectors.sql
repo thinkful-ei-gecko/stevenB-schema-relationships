a914a008e9cf8a55dfb9e18932aea79ee942b4a9
Steven Bull <stevenwbulljr@gmail.com>
2019-10-07 13:30:20 -0500

    finish 3rd query

--- query-selectors.sql
+++ query-selectors.sql
@@ -29,6 +29,27 @@ WHERE
   p.project_name = 'Plan christmas party';
 
 /* Third Query */
+SELECT
+  e.emp_name,
+  p.project_name
+FROM
+  employee e
+  JOIN
+  employee_project ep
+  ON 
+    ep.emp_id = e.id
+  JOIN 
+  project p
+  ON 
+    ep.project_id = p.id
+  JOIN
+  department d 
+  ON 
+    e.department = d.id
+WHERE 
+  p.project_name = 'Watch paint dry'
+AND
+  d.dept_name = 'Warehouse';
 
 /* Fourth Query */
 
